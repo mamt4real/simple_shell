9aa0d83edaeb7053906324b70906a1ca87ec602b
mamt4real <mamt4real@gmail.com>
2022-08-31 08:46:18 -0700

    dummy funs for and and or

--- 9-logic_funcs.c
+++ 9-logic_funcs.c
@@ -26,3 +26,73 @@ int execute_norm(char *args, shell_t *var)
 	free(args);
 	return (status);
 }
+
+/**
+ * execute_and - Handles execution with logical &&
+ * @args: command arguement to be executed
+ * @var: global shell variable
+ *
+ * Return; 
+ */
+
+int execute_and(char *args, shell_t *var)
+{
+	(void)args;
+	(void)var;
+
+	return (0);
+}
+
+/**
+ * execute_or - Handles execution with logical ||
+ * @args: command arguement to be executed
+ * @var: global shell variable
+ *
+ * Return; 
+ */
+
+int execute_or(char *args, shell_t *var)
+{
+	(void)args;
+	(void)var;
+
+	return (0);
+}
+
+/**
+ * logic_token - split a string base on a string
+ * @str: string to split
+ * Return: pointer to new or NULL
+ */
+char **logic_token(char *str)
+{
+	int j = 0, i = 0;
+	char **res;
+
+	if (!str)
+		return (0);
+	res = malloc(3 * sizeof(char *));
+	for (; str[i]; i++)
+	{
+		j = startsWith(str + i, "&&");
+		if (j)
+		{
+			res[0] = _strndup(str, i);
+			res[1] = _strdup("&&");
+			res[2] = str + i + j;
+			return (res);
+		}
+		j = startsWith(str + i, "||");
+		if (j)
+		{
+			res[0] = _strndup(str, i);
+			res[1] = _strdup("||");
+			res[2] = str + i + j;
+			return (res);
+		}
+	}
+	res[0] = _strdup(str);
+	res[1] = 0;
+	res[2] = 0;
+	return (res);
+}
