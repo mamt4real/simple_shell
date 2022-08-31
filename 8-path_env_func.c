a742cd5bd2fa8f0048511fd58c0d5410ca858991
mamt4real <mamt4real@gmail.com>
2022-08-31 10:06:57 -0700

    set and unset env funcs

--- 8-path_env_func.c
+++ 8-path_env_func.c
@@ -59,3 +59,91 @@ char *check_path(char *file)
 	free_tokenized(paths);
 	return (res);
 }
+
+/**
+ * _setenv - sets an environment variable
+ *
+ * @key: key of the variable
+ * @val: value of the variable
+ * Return: 0 success -1 failed
+ */
+int _setenv(char *key, char *val)
+{
+	int i = 0;
+	char *key_val, **temp;
+
+	if (!key || !key[0] || is_delimeter(key, '='))
+		return (-1);
+	/* reserve space for env */
+	key_val = malloc(_strlen(key) + _strlen(val) + 2);
+	if (key_val)
+		return (-1);
+	key_val[0] = '\0';
+	key_val = _strcat(key_val, key);
+	key_val = _strcat(key_val, "=");
+	key_val = _strcat(key_val, val);
+	/* check if key exist and overwrite */
+	while (environ[i])
+	{
+		if (startsWith(environ[i], key))
+		{
+			free(environ[i]);
+			environ[i] = key_val;
+			return (0);
+		}
+		i++;
+	}
+	/* key is new add it */
+	temp = malloc((i + 2) * sizeof(char *));
+	if (!temp)
+		return (-1);
+	for (i = 0; environ[i]; i++)
+		temp[i] = environ[i];
+	free_tokenized(environ);
+	environ = temp;
+	environ[i] = key_val;
+	environ[i + 1] = NULL;
+	return (0);
+}
+
+/**
+ * _unsetenv - unsets an environment variable
+ *
+ * @key: key of the variable
+ * Return: 0 success -1 failed
+ */
+int _unsetenv(char *key)
+{
+	int i = 0, j = 0, index = -1;
+	char *key_val, **temp;
+
+	if (!key || !key[0] || is_delimeter(key, '='))
+		return (-1);
+	/* check if key exist and save the index */
+	while (environ[i])
+	{
+		if (startsWith(environ[i], key))
+			index = i;
+		i++;
+	}
+	/* if key is not found */
+	if (index == -1)
+		return (0);
+	/* key is found remove it */
+	temp = malloc(i * sizeof(char *));
+	if (!temp)
+		return (-1);
+	for (i = 0; environ[i]; j++, i++)
+	{
+		if (i == index)
+		{
+			j--;
+			continue;
+		}
+		temp[j] = environ[i];
+	}
+	free_tokenized(environ);
+	temp[j] = NULL;
+	environ = temp;
+	return (0);
+}
